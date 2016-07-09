09a1e6c599b9b448e604872b6f5e33fd5ed5e41b
aelster <andy@elsternet.com>
2016-07-09 08:14:22 +0000

    Default to the current file, accept argument to make it generic

--- assignment_7/homemade-cat.py
+++ assignment_7/homemade-cat.py
@@ -3,6 +3,8 @@
 #def printFile(filePath): f = file(filePath) contents = f.read() printContents()
 #def printContents(): print("Contents of " + filePath) print(contents)
 
+import sys
+
 def printFile(filePath):
     f = open(filePath,'r')
     contents = f.read()
@@ -10,4 +12,12 @@ def printFile(filePath):
     print(contents)
 
 #================================================================================
-printFile( "/home/vagrant/intro-programming/assignment_7/homemade-cat.py" )
+#printFile( "/home/vagrant/intro-programming/assignment_7/homemade-cat.py" )
+
+if __name__ == "__main__":
+    file = __file__
+    if len(sys.argv) > 1:
+        file = sys.argv[1]
+
+    printFile( file )
+
